b08b86f76b7fa2e869e98f7f22480254d551a878
Rachelh7 <yulanh7@gmail.com>
2024-04-05 10:04:39 +1100

    new generateMeta function

---
+++ app.js
@@ -0,0 +1,10 @@
+const readline = require('readline')
+const { generateMeta } = require('./controllers/openaiController')
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+
+rl.question('YouTube Video Title: \n', generateMeta)
---
+++ controllers/openaiController.js
@@ -0,0 +1,34 @@
+const OpenAI = require("openai");
+const openai = new OpenAI({ apiKey: process.env.OPEN_AI_KEY });
+
+const generateMeta = async (title) => {
+  const description = await openai.chat.completions.create({
+    messages: [
+      {
+        role: 'user',
+        content: `Come up with a description for a YouTube video called ${title}`
+      }
+    ],
+    max_tokens: 100,
+    model: "gpt-3.5-turbo",
+  });
+
+
+  console.log(description.choices[0].message)
+
+
+  const tags = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [{
+      role: 'user',
+      content: `come up with 10 keywords for a YouTube video called ${title}`
+    }],
+    max_tokens: 100
+  })
+
+  console.log(tags.choices[0].message)
+
+}
+
+module.exports = { generateMeta }
+
