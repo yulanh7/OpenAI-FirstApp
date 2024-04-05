b0fde7134b4ceeeabdde209064c215070cf48ec0
Rachelh7 <yulanh7@gmail.com>
2024-04-05 11:21:52 +1100

    address the cross-origin issue

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const { generateMeta, generateImage } = require('./controllers/openaiController')
+const express = require('express');
+const cors = require('cors');
 
-// app setup
-const app = express()
-app.listen(4000, () => console.log('listening to requests on port 4000'))
+const { generateMeta, generateImage } = require('./controllers/openaiController');
 
-// middleware
-app.use(express.json())
+const app = express();
+app.listen(4000, () => console.log('listening to requests on port 4000'));
 
-// routes
-app.post('/openai/meta', generateMeta)
-app.post('/openai/image', generateImage)
+app.use(express.json());
+app.use(cors()); // Enable CORS for all routes
+
+app.post('/openai/meta', generateMeta);
+app.post('/openai/image', generateImage);
